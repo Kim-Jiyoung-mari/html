73802a48a586ffec98a8d8bc2346e922ae2f1f63
alsn8539 <alsn8539@gmail.com>
2025-02-10 16:14:48 +0900

    안다르(학원-백업)

--- portfolio/andar/script/index.js
+++ portfolio/andar/script/index.js
@@ -11,7 +11,7 @@ const mainSwiper = new Swiper('.main_swiper',{
     },
 });
 
-
+// 2행 에어쿨링
 const contentSwiper = new Swiper('.c_swiper',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
@@ -21,11 +21,12 @@ const contentSwiper = new Swiper('.c_swiper',{
         el: " .row2 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row2 .s_btn .next",
+        prevEl: ".row2 .s_btn .prev",
     }
 });
 
+// 3행 신상
 const contentSwiper2 = new Swiper('.c_swiper2',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
@@ -35,11 +36,12 @@ const contentSwiper2 = new Swiper('.c_swiper2',{
         el: " .row3 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row3 .s_btn .next",
+        prevEl: ".row3 .s_btn .prev",
     }
 });
 
+//베스트셀러
 const contentSwiper3 = new Swiper('.c_swiper3',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
@@ -49,7 +51,7 @@ const contentSwiper3 = new Swiper('.c_swiper3',{
         el: " .row4 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row4 .s_btn .next",
+        prevEl: ".row4 .s_btn .prev",
     }
 });
