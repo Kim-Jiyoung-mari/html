a6816f3caa78e5c92e6110204e2458ba1e90bd4b
alsn8539 <alsn8539@gmail.com>
2025-07-14 16:43:02 +0900

    클론-지그재그작업중

---
+++ portfolio/clone/script/index2.js
@@ -0,0 +1,21 @@
+//메인배너
+const pageAll = document.querySelector('.all_page')
+const pageCurrent = document.querySelector('.current_page');
+const pageSlide = document.querySelectorAll('.main_swiper .slide_css');
+
+pageAll.textContent = pageSlide.length;
+const mainBnr = new Swiper('.main_swiper',{
+    autoplay:{delay:4000,},
+    loop:true,
+    on:{
+        slideChange:function(){
+            pageCurrent.textContent = this.realIndex+1;
+        }
+    },
+})
+
+// // 첫구매 혜택 슬라이드
+// const beneFit = new Swiper('.benefit_horizontal',{
+//     slidesPerView:3,
+//     spaceBetween:3,
+// })
